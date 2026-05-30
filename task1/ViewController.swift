42065767a8a7c2c9cb447d974137f0801125a1d6
Noa Gilboa <rotemgilboa2@gmail.com>
2026-05-30 14:44:32 +0300

    Initial Commit

---
+++ task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by Noa Gilboa on 30/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ task1Tests/task1Tests.swift
@@ -0,0 +1,19 @@
+//
+//  task1Tests.swift
+//  task1Tests
+//
+//  Created by Noa Gilboa on 30/05/2026.
+//
+
+import Testing
+@testable import task1
+
+struct task1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
